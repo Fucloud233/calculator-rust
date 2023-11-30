56225e9ba4c823e4bb002a9a861a8045de5ec3ee
kingki663 <1581690775@qq.com>
2023-11-30 13:20:40 +0800

    🧪 test: test handle_sentence

--- src/test/calculator/unit.rs
+++ src/test/calculator/unit.rs
@@ -1,10 +1,33 @@
-use crate::test::utils::{line_test_runner, new_value, new_operation};
-use crate::ast::{ID, Line, Operator};
+use crate::ast::{Expr, Line, Operator, ID};
+use crate::calculator::Calculator;
+use crate::test::utils::{line_test_runner, new_ascii, new_greek, new_operation, new_value};
+use std::f64;
 
 #[test]
 fn test_parse_line() {
     line_test_runner(vec![
-        ("1 + 1", Line::Expression(new_operation(new_value(1.), new_value(1.), Operator::Plus))),
-        ("a = 1", Line::Sentence(ID::ASCII('a'), new_value(1.)))
+        (
+            "1 + 1",
+            Line::Expression(new_operation(new_value(1.), new_value(1.), Operator::Plus)),
+        ),
+        ("a = 1", Line::Sentence(ID::ASCII('a'), new_value(1.))),
     ]);
+}
+
+#[test]
+fn test_handle_sentence() {
+    // new a Calculator
+    let mut test_cal = Calculator::new();
+
+    let cases = vec![
+        (ID::ASCII('a'), new_value(1.0)),
+        (
+            ID::ASCII('b'),
+            new_operation(new_value(1.0), new_value(2.0), Operator::Plus),
+        ),
+    ];
+
+    cases
+        .into_iter()
+        .for_each(|(id, expr)| assert_eq!(test_cal.handle_sentence(&id, &expr), Ok(())))
 }
--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,14 +1,14 @@
+use crate::ast::ID;
 use lalrpop_util::lexer::Token;
 use lalrpop_util::ParseError;
-use std::io::Error as IO_Error;
 
+#[derive(PartialEq, Debug)]
 pub enum CalculatorError<'input> {
-    IOError(IO_Error),
     /// I don't know what will happen here when use 'static lifetime
     ParseError(ParseError<usize, Token<'input>, &'static str>),
 
     // you can define other types of error
     ArithmeticError(&'input str),
     OverflowError,
-    UndefinedIdError(&'input str),
+    UndefinedIdError(ID),
 }
