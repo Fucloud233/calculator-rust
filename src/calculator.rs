604849cbd6b634a1459238c7722165b437c8d945
fucloud-win11 <270928474@qq.com>
2023-11-30 23:39:15 +0800

    refactor: decouple handle_operation

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
+use std::f64;
+
+use lalrpop_util::lalrpop_mod;
 
 use crate::ast::{Expr, Line, Operator, ID};
 use crate::utils::error::CalculatorError;
-use lalrpop_util::lalrpop_mod;
-use std::f64;
+
 pub struct Calculator {
     symbol_table: HashMap<ID, f64>,
 }
@@ -11,12 +13,11 @@ pub struct Calculator {
 // type ParseError = lalrpop_util::ParseError<usize, Token<'_>, &str>;
 
 impl Calculator {
+
     /* --------------- init --------------- */
 
     pub fn new() -> Self {
-        Calculator {
-            symbol_table: HashMap::new(),
-        }
+        Calculator { symbol_table: HashMap::new() }
     }
 
     fn before_calculate(&mut self) {
@@ -24,14 +25,13 @@ impl Calculator {
         self.symbol_table.clear();
     }
 
-    /* --------------- calculator --------------- */
-    // return error type to be determined
-    pub fn calculate_expr<'input>(
-        &mut self,
-        line: &'input str,
-    ) -> Result<f64, CalculatorError<'input>> {
-        self.before_calculate();
 
+    /* --------------- calculator --------------- */
+
+    // return error type to be determined
+    pub fn calculate_expr<'input>(&mut self, line: &'input str) -> Result<f64, CalculatorError<'input>> {
+        self.before_calculate();
+        
         let parser_result = parse_line(line)?;
 
         if let Line::Expression(expr) = parser_result {
@@ -44,14 +44,11 @@ impl Calculator {
 
     // [Notice] reading and calculating must be decoupled
     // otherwise it will cause lifetime error
-    pub fn calculate_file<'input>(
-        &mut self,
-        lines: Vec<&'input str>,
-    ) -> Result<Vec<f64>, CalculatorError<'input>> {
+    pub fn calculate_file<'input>(&mut self, lines: Vec<&'input str>) -> Result<Vec<f64>, CalculatorError<'input>> {
         // init the status
         self.before_calculate();
 
-        // using result array
+        // using result array 
         // to decouple computation and output
         let mut results: Vec<f64> = Vec::new();
         for line in lines {
@@ -62,8 +59,8 @@ impl Calculator {
                 Line::Expression(expr) => {
                     let value = self.handle_expression(&expr)?;
                     results.push(value);
-                }
-                Line::Sentence(id, expr) => self.handle_sentence(&id, &expr)?,
+                },
+                Line::Sentence(id, expr) => self.handle_sentence(&id, &expr)?
             }
         }
 
@@ -88,57 +85,63 @@ impl Calculator {
                 }
             },
             Expr::Value(value) => Ok(*value),
-            Expr::Operation { l, r, opt } => {
-                let left = self.handle_expression(l)?;
-                let right = self.handle_expression(r)?;
+            Expr::Operation { l, r, opt } => self.handle_operation(l, r, opt),
+        }
+    }
 
-                // TODO deal with overflow error
-                match opt {
-                    Operator::Plus => Ok(left + right),
-                    Operator::Sub => Ok(left - right),
-                    Operator::Mul => Ok(left * right),
-                    Operator::Div => {
-                        if right == 0.0 {
-                            Err(CalculatorError::ArithmeticError("Division by zero"))
-                        } else {
-                            Ok(left / right)
-                        }
-                    }
-                    Operator::Power => Ok(left.powf(right)),
-                    Operator::Root => {
-                        if right == 0.0 {
-                            Err(CalculatorError::ArithmeticError("Root with zero index"))
-                        } else if right < 0.0 {
-                            Err(CalculatorError::ArithmeticError("Root with negative index"))
-                        } else if left < 0.0 && right % 2.0 == 0.0 {
-                            Err(CalculatorError::ArithmeticError(
-                                "Odd root of a negative number",
-                            ))
-                        } else {
-                            Ok(left.powf(right.recip()))
-                        }
-                    }
-                    Operator::Log => {
-                        if left <= 0.0 || right <= 0.0 {
-                            Err(CalculatorError::ArithmeticError(
-                                "Log with zero base or zero argument",
-                            ))
-                        // NOTE can't use match on float type!
-                        // NOTE use log2 ,ln and log10 to get more precise result, maybe use other crates future
-                        } else if left == 2.0 {
-                            Ok(right.log2())
-                        } else if left == f64::consts::E {
-                            Ok(right.ln())
-                        } else if left == 10.0 {
-                            Ok(right.log10())
-                        } else {
-                            Ok(right.log(left))
-                        }
-                    }
+    #[inline]
+    fn handle_operation<'input>(&mut self, l: &Expr, r: &Expr, opt: &Operator) -> Result<f64, CalculatorError<'input>> {
+        let left = self.handle_expression(l)?;
+        let right = self.handle_expression(r)?;
+
+        // TODO deal with overflow error
+        match opt {
+            Operator::Plus => Ok(left + right),
+            Operator::Sub => Ok(left - right),
+            Operator::Mul => Ok(left * right),
+            Operator::Div => {
+                if right == 0.0 {
+                    Err(CalculatorError::ArithmeticError("Division by zero"))
+                } else {
+                    Ok(left / right)
                 }
+            }
+            Operator::Power => Ok(left.powf(right)),
+            Operator::Root => {
+                if right == 0.0 {
+                    Err(CalculatorError::ArithmeticError("Root with zero index"))
+                } else if right < 0.0 {
+                    Err(CalculatorError::ArithmeticError("Root with negative index"))
+                } else if left < 0.0 && right % 2.0 == 0.0 {
+                    Err(CalculatorError::ArithmeticError(
+                        "Odd root of a negative number",
+                    ))
+                } else {
+                    Ok(left.powf(right.recip()))
+                }
+            }
+            Operator::Log => {
+                if left <= 0.0 || right <= 0.0 {
+                    return Err(CalculatorError::ArithmeticError(
+                        "Log with zero base or zero argument",
+                    ))
+                } 
+
+                // NOTE can't use match on float type!
+                // NOTE use log2 ,ln and log10 to get more precise result, maybe use other crates future
+                Ok(if left == 2.0 {
+                    right.log2()
+                } else if left == f64::consts::E {
+                    right.ln()
+                } else if left == 10.0 {
+                    right.log10()
+                } else {
+                    right.log(left)
+                })
             }
         }
     }
+
 
     pub(crate) fn handle_sentence<'input>(
         &mut self,
