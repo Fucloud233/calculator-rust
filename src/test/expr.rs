c841778a2a006d7695341f6b2ab18462444409d6
kingki663 <1581690775@qq.com>
2023-12-03 12:42:42 +0800

    🧪 test:test unary_operation_expr

--- src/test/expr.rs
+++ src/test/expr.rs
@@ -1,40 +1,44 @@
-use crate::ast::{Greek, Operator};
-use crate::test::utils::{expr_test_runner, new_operation, new_arithmetic, new_ascii, new_greek};
+use crate::ast::{Greek, Operator, UnaryOperator};
+use crate::test::utils::{expr_test_runner, new_arithmetic, new_ascii, new_greek, new_operation};
+
+use super::utils::{new_unary_operation, new_value};
 
 // test expr parser
 #[test]
 fn expr_test() {
-
     let cases = vec![
-    ("12+23", new_arithmetic(12., 23., Operator::Plus)),
-    ("12-23", new_arithmetic(12., 23., Operator::Sub)),
-    (
-        "a + b",
-        new_operation(
-            new_ascii('a'),
-            new_ascii('b'),
-            Operator::Plus
-        )
-    ),(
-        "a + b - c",
-        new_operation(
+        ("12+23", new_arithmetic(12., 23., Operator::Plus)),
+        ("12-23", new_arithmetic(12., 23., Operator::Sub)),
+        (
+            "a + b",
+            new_operation(new_ascii('a'), new_ascii('b'), Operator::Plus),
+        ),
+        (
+            "a + b - c",
             new_operation(
-                new_ascii('a'),
-                new_ascii('b'),
-                Operator::Plus
+                new_operation(new_ascii('a'), new_ascii('b'), Operator::Plus),
+                new_ascii('c'),
+                Operator::Sub,
             ),
-            new_ascii('c'),
-            Operator::Sub
-        )
-    ),(
-        "\\alpha - \\beta",
-        new_operation(
-            new_greek(Greek::Alpha),
-            new_greek(Greek::Beta),
-            Operator::Sub
-        )
-    )];
-    
+        ),
+        (
+            "\\alpha - \\beta",
+            new_operation(
+                new_greek(Greek::Alpha),
+                new_greek(Greek::Beta),
+                Operator::Sub,
+            ),
+        ),
+        (
+            "3!",
+            new_unary_operation(new_value(3.), UnaryOperator::Factorial),
+        ),
+        (
+            "sin1",
+            new_unary_operation(new_value(1.), UnaryOperator::Sin),
+        ),
+    ];
+
     expr_test_runner(cases);
 }
 
@@ -45,13 +49,9 @@ fn order_test() {
         "a + b * c",
         new_operation(
             new_ascii('a'),
-            new_operation(
-                new_ascii('b'),
-                new_ascii('c'), 
-                Operator::Mul
-            ),
-            Operator::Plus
-        )
+            new_operation(new_ascii('b'), new_ascii('c'), Operator::Mul),
+            Operator::Plus,
+        ),
     )];
 
     expr_test_runner(cases);
