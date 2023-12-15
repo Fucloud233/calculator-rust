7ebefa5d00e40d66545b8bc407ddc1c370be6458
fucloud-win11 <270928474@qq.com>
2023-12-15 18:53:08 +0800

    refactor: cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,10 @@
 use crate::calculator::Calculator;
-use crate::interactive::{interactive_mode, file_interactive_mode};
-use colored::*;
+use crate::interactive::interactive_mode;
+use crate::utils::error::CalculatorError;
 use std::env;
 use std::fs;
-use std::io::{self, Write};
 use std::path::PathBuf;
+use colored::*;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -22,72 +22,54 @@ pub struct Opt {
     interactive: bool,
 }
 
+fn print_value(value: f64) {
+    println!("{} {}", "Result => :".green(), value.to_string().bold())  
+}
+
+fn print_error(error: CalculatorError) {
+    // TODO: perfect error display
+    eprintln!("{} {}", "An error occurred => :".red(), error.message().bold())  
+}
+
 pub fn run_cli() {
     let opt = Opt::from_args();
-    match opt {
-        Opt {
-            expression: Some(expr),
-            ..
-        } => {
-            let mut calculator = Calculator::new();
-            match calculator.handle_line(&expr, false) {
-                Ok(Some(result)) => {
-                    println!("{} {}", "Result => :".green(), result.to_string().bold());
-                }
-                Ok(None) => {
-                    println!(
-                        "{}",
-                        "Statement executed successfully, but no value returned.".green()
-                    );
-                }
-                Err(e) => {
-                    println!("{} {}", "An error occurred => :".red(), e);
-                }
-            }
+
+    if let Some(expr) = opt.expression {
+        let mut calculator = Calculator::new();
+        match calculator.calculate_expr(&expr) {
+            Ok(value) => print_value(value),
+            Err(error) => print_error(error)
         }
-        Opt {
-            path: Some(file_path),
-            ..
-        } => {
-            println!("Reading file at: {}", file_path.display().to_string().cyan());
-            if let Ok(contents) = fs::read_to_string(&file_path) {
+    } else if let Some(file_path) = opt.path {
+        println!("Reading file at: {}", file_path.display().to_string().cyan());
+        let lines = match fs::read_to_string(&file_path) {
+            Ok(contents) => {
                 println!("File contents:\n{}", contents.yellow());
-                let lines: Vec<&str> = contents.lines().collect();
-                let mut calculator = Calculator::new();
+                contents.lines().collect::<Vec<&str>>()
+            }, 
+            Err(_) => {
+                eprintln!("{}", "Error reading file".red());
+                return
+            }
+        };
             
-                match calculator.calculate_file(lines) {
-                    Ok(results) => {
-                        for result in results {
-                            println!("{} {}", "Result => :".green(), result.to_string().bold());
-                        }
-                    }
-                    Err(e) => {
-                        println!("{} {}", "Error => :".red(), e);
-                    }
-                }
-            } else {
-                println!("{}", "Error reading file".red());
+        match Calculator::new().calculate_file(lines) {
+            Ok(values) => values.iter().for_each(|value| print_value(*value)),
+            Err(error) => print_error(error)
+        }
+    } else if opt.interactive {
+        interactive_mode();
+    } else {
+        match env::current_dir() {
+            Ok(path) => 
+                println!("{} {}", "Current directory:".blue(), path.display().to_string().blue()),
+            Err(e) => {
+                eprintln!("Error getting current directory: {}", e.to_string().red());
+                return
             }
         }
-        Opt {
-            interactive: true, ..
-        } => {
-            interactive_mode();
-        }
-        _ => {
-            match env::current_dir() {
-                Ok(path) => {
-                    println!("{} {}", "Current directory:".blue(), path.display().to_string().blue());
-                }
-                Err(e) => {
-                    eprintln!("Error getting current directory: {}", e.to_string().red());
-                    return;
-                }
-            }
-            println!(
-                "{}",
-                "Usage: calc -e <expression> | -p <path> | -i | -h".bright_blue()
-            );
-        }
+        println!(
+            "{}", "Usage: calc -e <expression> | -p <path> | -i | -h".bright_blue()
+        );
     }
 }
